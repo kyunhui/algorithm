23906b51e7581a36ed06299716055c702063e43a
kyunhui <106079352+kyunhui@users.noreply.github.com>
2023-07-04 17:10:00 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4040 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+    queue<int> q;
+    int n;
+    cin >> n;
+    for (int i = 1; i < n+1; i++)
+    {
+        q.push(i);
+    }
+    while (true)
+    {
+        if(q.size()==1) break;
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+    cout << q.front() << endl;
+    
+    
+}
