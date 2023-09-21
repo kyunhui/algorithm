1c3b11431702a1f1e46f273e72dbe1fd80963f7f
kyunhui <106079352+kyunhui@users.noreply.github.com>
2023-09-21 20:16:49 +0900

    [unrated] Title: A 강조하기, Time: 0.05 ms, Memory: 33.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
@@ -0,0 +1,7 @@
+function solution(myString) {
+    var answer = '';
+    myString = myString.toLowerCase()
+    myString = myString.replaceAll("a", "A")
+    answer = myString
+    return answer;
+}
