8d998ec5b594ad1ea5af474f036081a293546015
YunHui Kim <106079352+kyunhui@users.noreply.github.com>
2024-12-07 16:10:49 +0900

    [Silver I] Title: 회의실 배정, Time: 2756 ms, Memory: 49096 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,19 +1,19 @@
-N = int(input())
+n = int(input())
 time = []
 
-for _ in range(N):
-  start, end = map(int, input().split())
-  time.append([start, end])
+for _ in range(n):
+    a, b = map(int, input().split())
+    time.append([a, b])
 
-time = sorted(time, key=lambda a: a[0]) # 시작 시간을 기준으로 오름차순
-time = sorted(time, key=lambda a: a[1]) # 끝나는 시간을 기준으로 다시 오름차순
+time.sort(key=lambda a : a[0])
+time.sort(key=lambda a : a[1])
 
-last = 0 # 회의의 마지막 시간을 저장할 변수
-conut = 0 # 회의 개수를 저장할 변수
+last = 0 
+count = 0 
 
-for i, j in time:
-  if i >= last: # 시작시간이 회의의 마지막 시간보다 크거나 같을경우
-    conut += 1
-    last = j
-
-print(conut)
+for a, b in time:
+    if a >= last:
+        count += 1
+        last = b
+        
+print(count)
