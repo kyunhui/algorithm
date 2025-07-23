f9964c6f0bf226bceb5f395981ff780cf5293e4e
Yunhui Kim <106079352+kyunhui@users.noreply.github.com>
2025-07-23 15:06:55 +0900

    [Silver II] Title: 영상처리, Time: 912 ms, Memory: 53304 KB -BaekjoonHub

--- 백준/Silver/21938. 영상처리/영상처리.py
+++ 백준/Silver/21938. 영상처리/영상처리.py
@@ -7,42 +7,35 @@ n, m = map(int, input().split())
 pixel = [list(map(int, input().split())) for _ in range(n)]
 T = int(input())
 
-new_pixel = [[0 for _ in range(m)] for _ in range(n)]
-visited = [[0 for _ in range(m)] for _ in range(n)]
+new_pixel = [[0] * m for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+answer = 0
 
 
 for i in range(n):
     for j in range(m):
-        r = pixel[i][j*3]
-        g = pixel[i][j*3+1]
-        b = pixel[i][j*3+2]
+        r, g, b = pixel[i][j*3:(j+1)*3]
         avg = (r + g + b) // 3
-        if avg >= T:
-            new_pixel[i][j] = 255
-        else:
-            new_pixel[i][j] = 0
+        new_pixel[i][j] = 255 if avg >= T else 0
 
+def bfs(i, j):
+    que = deque()
+    que.append((i, j))
+    visited[i][j] = True
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-
-def bfs(x, y):
-    queue = deque()
-    queue.append((x, y))
-    visited[x][y] = True
-
-    while queue:
-        cx, cy = queue.popleft()
-        for i in range(4):
-            nx = cx + dx[i]
-            ny = cy + dy[i]
+    while que:
+        x, y = que.popleft()
+        
+        for d in range(4):
+            nx, ny = x + dx[d], y + dy[d]
+            
             if 0 <= nx < n and 0 <= ny < m:
                 if new_pixel[nx][ny] == 255 and not visited[nx][ny]:
                     visited[nx][ny] = True
-                    queue.append((nx, ny))
+                    que.append((nx, ny))
 
-answer = 0
 for i in range(n):
     for j in range(m):
         if new_pixel[i][j] == 255 and not visited[i][j]:
