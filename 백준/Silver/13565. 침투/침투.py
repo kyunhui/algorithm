27c76d4c19b5c67225a068569a0836a5d6ab1a28
Yunhui Kim <106079352+kyunhui@users.noreply.github.com>
2025-07-30 19:34:12 +0900

    [Silver II] Title: 침투, Time: 504 ms, Memory: 39800 KB -BaekjoonHub

--- 백준/Silver/13565. 침투/침투.py
+++ 백준/Silver/13565. 침투/침투.py
@@ -1,3 +1,11 @@
+'''
+
+시간초과 
+각 열에 대해 bfs()를 계속 호출하면 visited 배열을 새로 초기화, 탐색 초기화 이슈 ->  O(n x m^2)
+모든 '0'의 위치를 먼저 큐에 넣고 bfs()를 한 번 호출 -> O(n x m)
+
+'''
+
 import sys 
 from collections import deque
 
@@ -13,14 +21,11 @@ dy = [0, 0, -1, 1]
 que = deque()
 
 def bfs():
-    answer = 'NO'
-    
     while que:
         x, y = que.popleft()
         
-        if x == n-1 and maze[x][y] == '0': # 침투성공
-            answer = 'YES' 
-            return answer 
+        if x == n-1: # 마지막 행이면 성공
+            return 'YES'
             
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
@@ -30,7 +35,7 @@ def bfs():
                     que.append((nx, ny))
                     visited[nx][ny] = True 
 
-    return answer 
+    return 'NO'
 
 for i in range(m):
     if maze[0][i] == '0':
