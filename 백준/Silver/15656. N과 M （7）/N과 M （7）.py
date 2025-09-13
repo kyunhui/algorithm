e00f37748dd27505d328e71cac441778cd687816
Yunhui Kim <106079352+kyunhui@users.noreply.github.com>
2025-09-13 14:50:52 +0900

    [Silver III] Title: N과 M (7), Time: 2016 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+num = list(map(int, input().split()))
+num.sort()
+cur = []
+
+def dfs(x):
+    if len(cur) == m:
+        print(*cur)
+        return 
+    
+    for i in range(n):
+        cur.append(num[i])
+        dfs(i)
+        cur.pop()
+        
+dfs(0)
